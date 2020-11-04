0c86575a3a868f23f04a776e536762e447e00651
Yuriy Kubekin <vkubekin@gmail.com>
2020-11-04 17:46:57 +0300

    2020.5.1.1

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 var Stimulsoft = require('./Scripts/stimulsoft.dashboards').Stimulsoft;
+Stimulsoft.ExternalLibrary.xmldoc = require('xmldoc');
+
+Stimulsoft.System.NodeJs.useWebKit = false;
+Stimulsoft.System.NodeJs.initialize();
+
 module.exports = Stimulsoft;
